fa896229d7f0585386686b42c9f804e8eadec953
alwaysdw <alwaydw@gmail.com>
2018-07-03 13:12:33 +0900

    first commit test

--- app/src/main/java/jang/dw/com/dwweather/MainActivity.java
+++ app/src/main/java/jang/dw/com/dwweather/MainActivity.java
@@ -2,17 +2,26 @@ package jang.dw.com.dwweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "DWWeather";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate()");
 
+        //2018.07.03
+        Init();
 
     }
 
+    public void Init(){
+        Log.d(TAG, "Init()");
+
+    }
 
     @Override
     protected void onResume() {
