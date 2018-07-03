1d0698f4dff0365ce38d6f0bb483e2761b1940a2
alwaysdw <alwaydw@gmail.com>
2018-07-03 11:50:41 +0900

    Initial commit

---
+++ app/src/main/java/jang/dw/com/dwweather/MainActivity.java
@@ -0,0 +1,30 @@
+package jang.dw.com.dwweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
+}
